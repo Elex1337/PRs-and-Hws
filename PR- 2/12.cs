5ae9676997fe1e6ec9ebb8d5eff2f05bda6cf969
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-07 04:24:17 +0600

    Create 12.cs

---
+++ PR- 2/12.cs
@@ -0,0 +1,15 @@
+using System;
+class Program
+{
+
+    public static void Main()
+    {
+        Console.Write("Введите радиус круга ");
+        double r = Convert.ToDouble(Console.ReadLine());
+        double l = 2 * Math.PI * r;
+        double S = Math.PI * Math.Pow(r, 2);
+        Console.WriteLine($"Окружность круга {l}");
+        Console.WriteLine($"Площадь круга {S}");
+    }
+}
+    
