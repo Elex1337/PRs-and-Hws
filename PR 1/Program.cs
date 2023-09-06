40d271ad7dffa6a4c4683706189f1526c21261a2
Алекс <@DESKTOP-1ANSOAM>
2023-09-06 18:51:36 +0600

    Добавьте файлы проекта.

---
+++ PR 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter name");
+        String name = Console.ReadLine();
+        Console.WriteLine($"Приветствую тебя - {name}");
+    }
+}
+
