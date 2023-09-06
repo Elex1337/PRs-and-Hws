ff32a1fd7b6fce38968aaa55df852242c12b7162
Elex1337 <114895360+Elex1337@users.noreply.github.com>
2023-09-07 03:52:30 +0600

    Create 10a.cs

---
+++ PR- 2/10a.cs
@@ -0,0 +1,15 @@
+using System;
+class Program
+{
+
+    public static void Main()
+    {
+        for (int i = 20; i < 35; i++)
+        {
+            Console.WriteLine(i);
+        }
+        }
+    
+   
+        }
+    
