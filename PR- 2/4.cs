4325ab42eed35cebd296e8cc6d23f96d05647a8e
Elex1337 <114895360+Elex1337@users.noreply.github.com>
2023-09-06 20:00:56 +0600

    Update 4.cs

--- PR- 2/4.cs
+++ PR- 2/4.cs
@@ -1,1 +1,11 @@
+using System;
 
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Days ");
+        int days = Convert.ToInt32(Console.ReadLine());
+        int weeks = days / 7;
+        Console.WriteLine(weeks);
+    }
