a9f8fc09a7d0e8faad90b82c68dd3a18be8a5d12
Elex1337 <114895360+Elex1337@users.noreply.github.com>
2023-09-06 19:48:04 +0600

    Update 3.cs

--- PR- 2/3.cs
+++ PR- 2/3.cs
@@ -1,1 +1,12 @@
+using System;
 
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Input centimeters");
+        double centimetres = Convert.ToDouble(Console.ReadLine());
+        double m = centimetres * 0.01;
+        Console.WriteLine($"Metres {m}");
+    }
+}
