549b45d88594753536c02f45c6a994e9128e1161
Elex1337 <114895360+Elex1337@users.noreply.github.com>
2023-09-06 20:35:07 +0600

    Update 5.cs

--- PR- 2/5.cs
+++ PR- 2/5.cs
@@ -1,1 +1,76 @@
+using System;
 
+class Program
+
+{
+    static int tens(int n)
+    {
+        int tens = n / 10;
+        return tens;
+
+    }
+    static int units(int n)
+    {
+        int units = n % 10;
+        return units;
+    }
+    static int digitsSum(int n)
+
+    {
+
+        int sum = 0;
+
+        while (n != 0)
+
+        {
+
+            sum += n % 10;
+
+            n /= 10;
+
+        }
+
+        return sum;
+
+    }
+
+    static int digitsMult(int n)
+
+    {
+
+        int mult = 1;
+
+        while (n != 0)
+
+        {
+
+            mult *= n % 10;
+
+            n /= 10;
+
+        }
+
+        return mult;
+
+    }
+
+    static void Main()
+
+    {
+
+        Console.Write("Enter the number: ");
+
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Tens " + tens(n));
+
+        Console.WriteLine("Units " + units(n));
+
+        Console.WriteLine("Sum of digits: " + digitsSum(n));
+
+        Console.WriteLine("Mult of digits: " + digitsMult(n));
+
+
+    }
+
+}
