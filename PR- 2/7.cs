d59da08e0f279a9e535a9873d0d3b19e1880a48a
Elex1337 <114895360+Elex1337@users.noreply.github.com>
2023-09-07 03:12:15 +0600

    Update 7.cs

--- PR- 2/7.cs
+++ PR- 2/7.cs
@@ -1,1 +1,27 @@
+using System;
+class Program
+{
+    public static void Main()
+    {
+        Console.Write("Radius ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("SideLenght ");
+        double sideLength = Convert.ToDouble(Console.ReadLine());
+        double circleArea = Math.PI * Math.Pow(radius, 2);
+        double squareArea = Math.Pow(sideLength, 2);
+        if (circleArea > squareArea)
+        {
+            Console.WriteLine("Площадь круга больше ");
+        }
+        else if (circleArea < squareArea)
+        {
+            Console.WriteLine("Площадь квадрата больше");
+        }
+        else
+        {
+            Console.WriteLine("Площади равны");
+        }
 
+
+    }
+}
