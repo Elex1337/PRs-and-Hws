687a4c3436cb9d0356c23516efbc4b16c7e3b6a6
Elex1337 <114895360+Elex1337@users.noreply.github.com>
2023-09-06 20:40:19 +0600

    Update 6.cs

--- PR- 2/6.cs
+++ PR- 2/6.cs
@@ -1,1 +1,15 @@
+using System;
+class Program
+{
+    public static void Main()
+    {
+        bool a = true;
+        bool b = false;
+     bool c = false;
+        Console.WriteLine(a ^ b);
+        Console.WriteLine(a & b);
+        Console.WriteLine(b ^ c);
 
+    }
+
+}
