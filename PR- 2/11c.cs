ba6a379479e0da814482aea1bf2063632ca8e53d
Elex1337 <114895360+Elex1337@users.noreply.github.com>
2023-09-07 03:55:23 +0600

    Create 11c.cs

---
+++ PR- 2/11c.cs
@@ -0,0 +1,16 @@
+using System;
+class Program
+{
+
+    public static void Main()
+    {
+        Console.Write("input ");
+        for (int i = Convert.ToInt32(Console.ReadLine()); i < 50; i++)
+        {
+            Console.WriteLine(Math.Pow(i, 3));
+        }
+        }
+    
+   
+        }
+    
