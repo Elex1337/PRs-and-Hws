a472fbf456fd67a2bf75779bb8b623d31e1073a8
Elex1337 <114895360+Elex1337@users.noreply.github.com>
2023-09-06 19:37:09 +0600

    Add files via upload

---
+++ PR- 2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        Console.WriteLine($"{a}  {b}  {c}");
+    }
+}
