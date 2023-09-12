677600821333f56e93d7dd7ad4a537c3cab57ba1
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-13 05:15:16 +0600

    Update 23.cs

--- PR- 2/23.cs
+++ PR- 2/23.cs
@@ -1,1 +1,24 @@
+using System;
+
+class Program
+{
+     static int  f(int h, int m, int s)
+    {
+        return h * 3600 + m * 60 + s;
+    }
+
+    static void Main()
+    {
+        Console.Write("Часы ");
+        int h = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Минуты ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Секунды ");
+        int s = Convert.ToInt32(Console.ReadLine());
+        int Totalsec = f(h, m, s);
+        Console.WriteLine($"Прошло  " +Totalsec +" секунд");
+    }
+}
+
+
 
