e6f827b1babace195496a6070523e29770eebb11
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-13 05:55:14 +0600

    Update 27.cs

--- PR- 2/27.cs
+++ PR- 2/27.cs
@@ -1,1 +1,24 @@
+using System;
+
+class Program
+{
+  static bool even(int n)
+    {
+        if (n % 2 == 0) 
+        { return true;
+        } else
+        {
+            return false;
+        } 
+    }
+
+    static void Main()
+    {
+        int n = 23;
+        even(n);
+        Console.WriteLine(even(n));
+    }
+}
+
+
 
