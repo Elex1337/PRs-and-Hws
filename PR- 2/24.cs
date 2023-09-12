9582a5c748573332cac0baf0aaa8e5ba4f671ffe
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-13 05:19:34 +0600

    Update 24.cs

--- PR- 2/24.cs
+++ PR- 2/24.cs
@@ -1,1 +1,22 @@
+using System;
+
+class Program
+{
+     static int  f(int m, int d)
+    {
+        return m * 30 + d ;
+    }
+
+    static void Main()
+    {
+        Console.Write("Месяцов ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Дней ");
+        int d = Convert.ToInt32(Console.ReadLine());
+        int Totaldays = f(m,d);
+        Console.WriteLine($"Прошло  " +Totaldays +" дней");
+    }
+}
+
+
 
