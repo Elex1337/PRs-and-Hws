84550b00510b1894394ac078ebd9cc9920355e95
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-13 05:02:13 +0600

    Update 22.cs

--- PR- 2/22.cs
+++ PR- 2/22.cs
@@ -1,1 +1,28 @@
+using System;
+
+class Program
+{
+      static int  f(int x, int y) 
+    {
+       if (x == 0 && y == 0)
+        {
+            return 0;
+        } else if (y == 0)
+        {
+            return 12/x;
+        } else
+        {
+            return 144/(x*y);
+        }
+
+    }
+
+    static void Main()
+    {
+        int x = 10;
+        int y = 5;
+        Console.WriteLine($"IDK " + f(x, y)); }
+    }
+
+ 
 
