20a234b661f27cc6fcd9926a22655a6c89df65d6
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-14 05:20:26 +0600

    Update 15.cs

--- PR- 2/HM2/15.cs
+++ PR- 2/HM2/15.cs
@@ -1,1 +1,13 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+      double a = Convert.ToInt32 (Console.ReadLine());
+      double x = 12*Math.Pow(a,2) + 7 * a - 16 ;
+        Console.WriteLine(x);
+    }
+}
+
 
