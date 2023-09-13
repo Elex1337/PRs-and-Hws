ed1590f18ac08034fb44e4a61d8c07ad507c1cf0
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-14 04:55:45 +0600

    Update 10.cs

--- PR- 2/HM2/10.cs
+++ PR- 2/HM2/10.cs
@@ -1,1 +1,15 @@
+using System;
 
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int year = 5;
+        int month = 10;
+        int day = 5;
+        int nextDay = (year*365+30*10+day*1) + 1;
+        int lastDay = (year * 365 + 30 * 10 + day * 1) - 1;
+        Console.WriteLine($"Next day {nextDay} last day {lastDay}");
+    }
+
+} 
