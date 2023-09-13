b09b1301a30a9153c88fffd8bb350e9239155ae2
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-14 05:06:58 +0600

    Update 12.cs

--- PR- 2/HM2/12.cs
+++ PR- 2/HM2/12.cs
@@ -1,1 +1,13 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(5);
+        Console.WriteLine(10);
+        Console.WriteLine(21);
+    }
+}
+
 
