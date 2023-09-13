89e54537ee0faedb3c1136c57e8e6e28c996057d
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-13 06:14:40 +0600

    Update 30.cs

--- PR- 2/30.cs
+++ PR- 2/30.cs
@@ -1,1 +1,27 @@
+using System;
+
+class Program
+{
+  static int f(int M, int N)
+    {
+        int sum = 0;
+        for (int i = M; i <= N; i++)
+        {
+            if (i % 2 != 0)
+            {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+
+    static void Main()
+    {
+        int M = 5;
+        int N = 25;
+        int result = f(M, N);
+        Console.WriteLine($"Сумма нечетных чисел от {M} до {N} равна {result}");
+    }
+}
 
