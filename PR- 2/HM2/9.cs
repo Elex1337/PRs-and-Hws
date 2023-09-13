fb0adba5925fd3885a678a16ebf5f38fb0c57d6c
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-14 04:39:43 +0600

    Update 9.cs

--- PR- 2/HM2/9.cs
+++ PR- 2/HM2/9.cs
@@ -1,1 +1,13 @@
+using System;
 
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double a = 2;
+        double b = 3;
+        double c = Math.Sqrt(a + b);
+        Console.WriteLine("Среднее ариф число " +(a + b)/2);
+        Console.WriteLine("Среднее геом число " +c);
+    }
+} 
