3d6e0e271415fca565c42658547dd61f091d63aa
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-13 06:59:12 +0600

    Update 5.cs

--- PR- 2/HM2/5.cs
+++ PR- 2/HM2/5.cs
@@ -1,1 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 11; i++)
+        {
+            double result = i * 0.453;
+            Console.WriteLine(result);  
+        }
+    }
+}
 
