d7762655bb5a57f1e9e80ba649379c6c76e4a381
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-14 05:14:47 +0600

    Update 13.cs

--- PR- 2/HM2/13.cs
+++ PR- 2/HM2/13.cs
@@ -1,1 +1,15 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int a, b, c;
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        c = Convert.ToInt32(Console.ReadLine());
+        Console.Write($"{a}  {b}  {c}");
+    }
+}
+
 
