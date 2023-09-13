123e084c7cbbf24810a6cccb1f19a3312dd65cb0
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-13 07:06:18 +0600

    Update 6.cs

--- PR- 2/HM2/6.cs
+++ PR- 2/HM2/6.cs
@@ -1,1 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 20;
+        for (int i = 0; i < 20; i++)
+        {
+            Console.Write(" " +a);
+        }
+    }
+}
 
