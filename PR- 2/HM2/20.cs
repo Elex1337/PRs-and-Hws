ff7843b6ef6e16a3debca66ef9654925c9ae90f5
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-14 05:30:26 +0600

    Update 20.cs

--- PR- 2/HM2/20.cs
+++ PR- 2/HM2/20.cs
@@ -1,1 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool x = false;
+        bool y = true;
+        bool z = false;
+        Console.WriteLine(x || z);
+        Console.WriteLine(x ^ y);
+        Console.WriteLine(x ^ z);
+    }
+}
 
