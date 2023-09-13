07fea8696441636b4f73a85a23c59fba1f5386d0
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-13 06:42:04 +0600

    Update 2.cs

--- PR- 2/HM2/2.cs
+++ PR- 2/HM2/2.cs
@@ -1,1 +1,24 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+
+
+        Console.Write("Введите значение B: ");
+        int B = Convert.ToInt32(Console.ReadLine());
+            for (int i = A; i <= B; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
 
