f959ca98c4e1c41268b25fba14adedd27b50d74c
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-13 06:50:56 +0600

    Update 3.cs

--- PR- 2/HM2/3.cs
+++ PR- 2/HM2/3.cs
@@ -1,1 +1,13 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i < 11; i++)
+        {
+            int result = 9 * i;
+            Console.WriteLine(result);  
+        }
+    }
+}
