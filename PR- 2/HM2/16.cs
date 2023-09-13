167e9186bd2d39350396f0f5602a65abb5942ccc
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-14 05:26:16 +0600

    Update 16.cs

--- PR- 2/HM2/16.cs
+++ PR- 2/HM2/16.cs
@@ -1,1 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double distanceInKilometers = 10; 
+        double distanceInFeet = 32808.4; 
+
+        double feetToKilometers = 0.000305; 
+
+        double distanceInKilometersFromFeet = distanceInFeet * feetToKilometers;
+
+        if (distanceInKilometers < distanceInKilometersFromFeet)
+        {
+            Console.WriteLine("Расстояние в километрах меньше.");
+        }
+        else if (distanceInKilometers > distanceInKilometersFromFeet)
+        {
+            Console.WriteLine("Расстояние в футах меньше.");
+        }
+        else
+        {
+            Console.WriteLine("Расстояния равны.");
+        }
+    }
+}
 
