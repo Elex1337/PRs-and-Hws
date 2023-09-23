19fd661e9a6ed4f0a682e9d7e91ad65c5294f7b4
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-23 17:28:51 +0600

    Update 1.cs

--- PR-3/HWork-3/1.cs
+++ PR-3/HWork-3/1.cs
@@ -1,1 +1,22 @@
-
+using System;
+class Program
+{
+    static void MaxValue(int[] arr)
+    {
+        int max = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            Console.WriteLine(i);
+            }
+        }
+    }
+        
+    static void Main()
+    {
+        int[] arr = new int[] { 2, 9, 6, 7 };
+        MaxValue(arr);
+    }
+}
