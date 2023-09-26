f87ca9aac8f9fef0b4649ba4e73e6f6c6149a5b8
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 01:21:38 +0600

    Update 15.cs

--- PR-3/HWork-3/15.cs
+++ PR-3/HWork-3/15.cs
@@ -1,1 +1,35 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите текст:");
+
+        string inputText = Console.ReadLine();
+        string convertedText = ConvertCase(inputText);
+
+        Console.WriteLine("Преобразованный текст:");
+        Console.WriteLine(convertedText);
+    }
+
+    static string ConvertCase(string text)
+    {
+        char[] charArray = text.ToCharArray();
+
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            if (char.IsLower(charArray[i]))
+            {
+                charArray[i] = char.ToUpper(charArray[i]);
+            }
+            else if (char.IsUpper(charArray[i]))
+            {
+                charArray[i] = char.ToLower(charArray[i]);
+            }
+        }
+
+        return new string(charArray);
+    }
+}
 
