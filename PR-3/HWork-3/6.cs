24e6cd894db045048bad3aa6dd56ebb7c8dfeb50
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 01:04:07 +0600

    Update 6.cs

--- PR-3/HWork-3/6.cs
+++ PR-3/HWork-3/6.cs
@@ -1,1 +1,24 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите символы (введите точку для завершения):");
+
+        int spaceCount = 0;
+        char inputChar;
+
+        do
+        {
+            inputChar = Console.ReadKey().KeyChar;
+            if (inputChar == ' ')
+            {
+                spaceCount++;
+            }
+        } while (inputChar != '.');
+
+        Console.WriteLine($"\nКоличество пробелов: {spaceCount}");
+    }
+}
 
