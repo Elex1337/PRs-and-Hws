5bdc6370848e0b7c75012e4858120b00cf8f0b61
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 00:57:18 +0600

    Update 10.cs

--- PR-3/10.cs
+++ PR-3/10.cs
@@ -1,1 +1,34 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку символов:");
+        string inputString = Console.ReadLine();
+
+        string[] words = inputString.Split(' ');
+
+        int count = 0;
+        foreach (string word in words)
+        {
+            if (IsFirstAndLastCharEqual(word))
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Количество слов с совпадающими первым и последним символами: {count}");
+    }
+
+    static bool IsFirstAndLastCharEqual(string word)
+    {
+        if (word.Length >= 2 && word[0] == word[word.Length - 1])
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
 
