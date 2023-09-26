3b7f32a42c7d7d7fea8eb37303a302099737f7c7
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 00:46:08 +0600

    Update 4.cs

--- PR-3/4.cs
+++ PR-3/4.cs
@@ -1,1 +1,22 @@
+using System;
+
+class Program
+{
+
+
+    static void Main()
+    
+    {
+        Console.WriteLine("Enter ");
+        string str = Console.ReadLine();
+        string[] words = str.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+        int wordsout = words.Length;
+        Console.WriteLine($"Количество слов в предложении: {wordsout}");
+
+    }
+
+}
+
+
+
 
