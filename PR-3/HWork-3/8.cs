7e4ca24f01ac27661241fc71e909a9a2da385976
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 01:06:39 +0600

    Update 8.cs

--- PR-3/HWork-3/8.cs
+++ PR-3/HWork-3/8.cs
@@ -1,1 +1,42 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string word = "класс";
+        string modifiedWord = ReplaceDuplicateLetters(word);
+
+        Console.WriteLine($"Исходное слово: {word}");
+        Console.WriteLine($"Слово с заменой: {modifiedWord}");
+    }
+
+    static string ReplaceDuplicateLetters(string word)
+    {
+        char[] letters = word.ToCharArray();
+
+        if (letters.Length != 5 ||
+            letters[0] != 'к' ||
+            letters[1] != 'л' ||
+            letters[2] != 'а' ||
+            letters[3] != 'с' ||
+            letters[4] != 'с')
+        {
+            Console.WriteLine("Слово должно быть 'класс'");
+            return word;
+        }
+
+        for (int i = 0; i < letters.Length - 1; i++)
+        {
+            if (letters[i] == letters[i + 1])
+            {
+                letters[i] = '1';
+                letters[i + 1] = '1';
+                break; 
+            }
+        }
+
+        return new string(letters);
+    }
+}
 
