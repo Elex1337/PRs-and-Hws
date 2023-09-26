84935b04b0e68dd46a6ad0a9d5c96e68551d9962
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 00:51:33 +0600

    Update 7.cs

--- PR-3/7.cs
+++ PR-3/7.cs
@@ -1,1 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку с прописными буквами русского алфавита:");
+        string input = Console.ReadLine();
+
+        HashSet<char> uniqueLetters = new HashSet<char>();
+
+        foreach (char c in input)
+        {
+            if (char.IsUpper(c) && IsRussianLetter(c))
+            {
+                uniqueLetters.Add(c);
+            }
+        }
+
+        int uniqueLetterCount = uniqueLetters.Count;
+
+        Console.WriteLine($"Количество различных прописных букв русского алфавита: {uniqueLetterCount}");
+    }
+
+    static bool IsRussianLetter(char c)
+    {
+        return (c >= 'А' && c <= 'Я') || (c >= 'а' && c <= 'я');
+    }
+}
 
