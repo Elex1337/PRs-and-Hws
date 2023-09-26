218dd31ec4fe5f5c5ea582a864475cbece37a97a
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 00:56:37 +0600

    Update 9.cs

--- PR-3/9.cs
+++ PR-3/9.cs
@@ -1,1 +1,46 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите текст из 20 символов:");
+        string inputText = Console.ReadLine();
+
+        if (inputText.Length != 20)
+        {
+            Console.WriteLine("Введена неверная длина строки. Пожалуйста, введите 20 символов.");
+            return;
+        }
+
+        if (CheckIfCanSpellName(inputText))
+        {
+            Console.WriteLine("Можно составить ваше имя:");
+            Console.WriteLine("Ваше имя");
+        }
+        else
+        {
+            Console.WriteLine("Нет имени");
+        }
+    }
+
+    static bool CheckIfCanSpellName(string inputText)
+    {
+        string yourName = "Нурболат";
+
+        foreach (char c in yourName)
+        {
+            if (inputText.Contains(c.ToString()))
+            {
+                inputText = inputText.Remove(inputText.IndexOf(c), 1);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
 
