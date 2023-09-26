40bcbc6ae48b9a0f569e74aeb704be6a98fa2697
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 01:02:12 +0600

    Update 5.cs

--- PR-3/HWork-3/5.cs
+++ PR-3/HWork-3/5.cs
@@ -1,1 +1,45 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку:");
+        string inputString = Console.ReadLine();
+
+        string[] words = inputString.Split(new char[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (words.Length < 2)
+        {
+            Console.WriteLine("Недостаточно слов для выполнения задачи.");
+            return;
+        }
+
+        string lastWord = words[words.Length - 1].ToLower();
+
+        string result = "";
+        foreach (string word in words)
+        {
+            if (!ContainsAnyLetterFromWord(word.ToLower(), lastWord))
+            {
+                result += word + " ";
+            }
+        }
+
+        Console.WriteLine("Результат:");
+        Console.WriteLine(result.Trim());
+    }
+
+    static bool ContainsAnyLetterFromWord(string word, string referenceWord)
+    {
+        foreach (char c in referenceWord)
+        {
+            if (word.Contains(c))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
 
