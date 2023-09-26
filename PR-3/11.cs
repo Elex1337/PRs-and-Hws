9a3cde5a957a3efd5d0af87553bfb407bd15afec
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 00:58:02 +0600

    Update 11.cs

--- PR-3/11.cs
+++ PR-3/11.cs
@@ -1,1 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите маленькую русскую букву:");
+        char smallLetter = Console.ReadKey().KeyChar;
+
+        if (char.IsLower(smallLetter) && smallLetter >= 'а' && smallLetter <= 'я')
+        {
+            char capitalLetter = (char)(smallLetter - ('а' - 'А'));
+            Console.WriteLine($"\nСоответствующая большая буква: {capitalLetter}");
+        }
+        else
+        {
+            Console.WriteLine("\nЭто не маленькая русская буква.");
+        }
+    }
+}
 
