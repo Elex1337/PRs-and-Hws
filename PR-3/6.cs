9a3342b97d360136a1a1cf0fb0df9ef90ae7851d
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 00:48:56 +0600

    Update 6.cs

--- PR-3/6.cs
+++ PR-3/6.cs
@@ -1,1 +1,41 @@
+using System;
+
+class Program
+{
+
+
+    static void Main()
+    
+    {
+        Console.WriteLine("Enter ");
+        string str = Console.ReadLine();
+        char currentChar = str[0];
+        int currentCount = 1;
+        int maxCount = 1;
+        for (int i = 1; i < str.Length; i++)
+        {
+            if (str[i] == currentChar)
+            {
+                currentCount++;
+            }
+            else
+            {
+                currentChar = str[i];
+                currentCount = 1;
+            }
+
+            if (currentCount > maxCount)
+            {
+                maxCount = currentCount;
+            }
+        }
+        Console.WriteLine(maxCount);
+
+    }
+
+}
+
+
+
+
 
