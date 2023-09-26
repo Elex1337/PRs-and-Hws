b665503248e37a75c9d025c8a9081ffa380438dd
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 01:13:23 +0600

    Update 10.cs

--- PR-3/HWork-3/10.cs
+++ PR-3/HWork-3/10.cs
@@ -1,1 +1,28 @@
+using System;
 
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите целое число N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        int reversedNumber = ReverseNumber(N);
+
+        Console.WriteLine("Число, прочитанное справа налево: " + reversedNumber);
+    }
+
+    static int ReverseNumber(int num)
+    {
+        int reversedNum = 0;
+
+        while (num > 0)
+        {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+
+        return reversedNum;
+    }
+}
