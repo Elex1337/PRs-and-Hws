e9f8257ea3e22f5b4789edd382a060e34ba9ae14
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 01:15:52 +0600

    Update 12.cs

--- PR-3/HWork-3/12.cs
+++ PR-3/HWork-3/12.cs
@@ -1,1 +1,35 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "Это пример текста с словом one. Другие слова могут тоже присутствовать.";
+        bool containsOne = CheckForWord(text, "one");
+
+        if (containsOne)
+        {
+            Console.WriteLine("Текст содержит слово 'one'.");
+        }
+        else
+        {
+            Console.WriteLine("Текст не содержит слово 'one'.");
+        }
+    }
+
+    static bool CheckForWord(string text, string word)
+    {
+        string[] words = text.Split(new char[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string w in words)
+        {
+            if (w.Equals(word, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
 
