a7b31d098940bd876c9d0568905285538af8854d
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 01:14:21 +0600

    Update 11.cs

--- PR-3/HWork-3/11.cs
+++ PR-3/HWork-3/11.cs
@@ -1,1 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string inputString = "Это/тест\\удаления/слэшей\\";
+        string modifiedString = RemoveSlashes(inputString);
+
+        Console.WriteLine("Исходная строка: " + inputString);
+        Console.WriteLine("Строка после удаления слэшей: " + modifiedString);
+    }
+
+    static string RemoveSlashes(string input)
+    {
+        string result = input.Replace("/", "").Replace("\\", "");
+        return result;
+    }
+}
 
