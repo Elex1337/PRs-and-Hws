c15a68d39eefc47b5781d5186797e0f1d425952f
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 01:16:56 +0600

    Update 13.cs

--- PR-3/HWork-3/13.cs
+++ PR-3/HWork-3/13.cs
@@ -1,1 +1,32 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string inputString = "Пример строки с буквой P. Еще одна буква P здесь.";
+        char targetChar = 'P';
+
+        int count = CountOccurrences(inputString, targetChar);
+
+        Console.WriteLine($"Количество вхождений буквы '{targetChar}' в строке: {count}");
+    }
+
+    static int CountOccurrences(string input, char target)
+    {
+        int count = 0;
+
+        foreach (char c in input)
+        {
+            if (Char.ToUpper(c) == Char.ToUpper(target))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+
 
