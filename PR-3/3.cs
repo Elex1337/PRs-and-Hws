536b28889f4bf21b7e549c172924e1a58b4494e7
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-26 23:47:31 +0600

    Update 3.cs

--- PR-3/3.cs
+++ PR-3/3.cs
@@ -1,1 +1,30 @@
+using System;
+using System.Diagnostics.Tracing;
+
+class Program
+{
+ static bool Check_Polyndrom(string str)
+    {
+       for (int i = 0; i < str.Length/2; i++) {
+        if (str[i] != str[str.Length - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+
+    }
+    static void Main()
+    {
+        string str = "idxc";
+     if (Check_Polyndrom(str))
+        {
+            Console.WriteLine("Polyndrom");
+        }
+     else
+        {
+            Console.WriteLine("Incorrect ");
+        }
+    }
+}
 
