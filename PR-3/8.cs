7d2c31aa04109a7912139e4322d23c4adfe689fa
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 00:53:25 +0600

    Update 8.cs

--- PR-3/8.cs
+++ PR-3/8.cs
@@ -1,1 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку из 20 символов:");
+        string inputString = Console.ReadLine();
+
+        if (inputString.Length != 20)
+        {
+            Console.WriteLine("Введена неверная длина строки. Пожалуйста, введите 20 символов.");
+            return;
+        }
+
+        int digitCount = 0;
+        foreach (char c in inputString)
+        {
+            if (char.IsDigit(c))
+            {
+                digitCount++;
+            }
+        }
+
+        Console.WriteLine($"Количество цифр в строке: {digitCount}");
+    }
+}
 
