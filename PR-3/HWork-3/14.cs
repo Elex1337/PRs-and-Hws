cba10c2e65a84c97cb9204bdc3a1f90845f203c2
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-09-27 01:20:07 +0600

    Update 14.cs

--- PR-3/HWork-3/14.cs
+++ PR-3/HWork-3/14.cs
@@ -1,1 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите текст:");
+        string inputText = Console.ReadLine();
+        string[] words = Regex.Split(inputText, @"\W+");
+
+        Dictionary<char, int> startingLetterCount = new Dictionary<char, int>();
+
+        foreach (string word in words)
+        {
+            if (!string.IsNullOrEmpty(word))
+            {
+                char firstLetter = char.ToUpper(word[0]); 
+                if (Char.IsLetter(firstLetter))
+                {
+                    if (startingLetterCount.ContainsKey(firstLetter))
+                    {
+                        startingLetterCount[firstLetter]++;
+                    }
+                    else
+                    {
+                        startingLetterCount[firstLetter] = 1;
+                    }
+                }
+            }
+        }
+
+        if (startingLetterCount.Count > 0)
+        {
+            char mostCommonLetter = startingLetterCount.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+            int count = startingLetterCount[mostCommonLetter];
+
+            Console.WriteLine($"Наиболее часто встречающаяся начальная буква: {mostCommonLetter}, количество слов: {count}");
+        }
+        else
+        {
+            Console.WriteLine("В тексте нет слов.");
+        }
+    }
+}
 
