06408c923a108a8a91d7d19b62f0d3a623069a73
Zhursinbek Nurbolat <114895360+Elex1337@users.noreply.github.com>
2023-10-02 18:11:26 +0600

    Add files via upload

---
+++ pr-4/HW-4/Project_1/Car.partial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Project_1
+{
+    partial class Car
+    {
+        public void Bevipkazakh()
+        {
+            Console.WriteLine("Toyota camry куплена ");
+        }
+
+    }
+}
+
